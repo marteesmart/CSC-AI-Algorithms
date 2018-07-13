6f3fbce69f47a84142c31f65039209061a835d76
Mayowa Aj <mayowaaj69@hotmail.co.uk>
2018-07-13 15:46:28 +0100

    added some starter files

---
+++ src/csc/ai/algorithms/search/uninformed/Bfs.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc.ai.algorithms.search.uninformed;
+
+import csc.ai.algorithms.search.ISearch;
+
+/**
+ *
+ * @author mayowa
+ */
+public class Bfs implements ISearch {
+    
+}
